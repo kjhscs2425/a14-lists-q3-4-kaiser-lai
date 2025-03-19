e701f2edd9c2a507421e28a53fb0807d9da70d5d
kaiser-lai <klai2027@kehillahstudent.org>
2025-03-19 12:40:17 -0700

    done

--- q4.py
+++ q4.py
@@ -4,6 +4,9 @@ def make_list_of_files(n):
     assume n greater than or equal to 1
     hint: use a loop and append
     """
-    return
+    file_list = []
+    for i in range(n):
+        file_list.append(f"file{i}.txt")
+    return file_list
 
 print(make_list_of_files(5)) #['file_0.txt', 'file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
